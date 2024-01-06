7b71c5955ffae055e8eef30f521a4ca409884805
Pham Thanh son (FIS PBDXGHN) <sonpt51@fpt.com>
2024-01-06 14:47:25 +0700

    van dang logic o controller

--- src/main/java/com/example/spring_jwt/controller/GoogleLoginController.java
+++ src/main/java/com/example/spring_jwt/controller/GoogleLoginController.java
@@ -1,8 +1,11 @@
 package com.example.spring_jwt.controller;
 
 import com.example.spring_jwt.dto.TokenRequest;
+import com.example.spring_jwt.model.google.UserInfo;
 import com.example.spring_jwt.security.JwtProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.NoSuchAlgorithmException;
@@ -15,9 +18,14 @@ public class GoogleLoginController {
     @Autowired
     private JwtProvider jwtProvider;
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @PostMapping("/login-google")
     public Object loginGoogle(@RequestBody TokenRequest crenditial) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        return jwtProvider.getAllClaimsFromToken(crenditial.getCrenditial());
+        UserInfo user = (UserInfo) jwtProvider.getAllClaimsFromToken(crenditial.getCrenditial());
+        UserDetails userDetails = userDetailsService.loadUserByUsername(user.getEmail());
+        return jwtProvider.generateToken(userDetails);
     }
 
 }
